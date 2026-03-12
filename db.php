6a1df2c3f6496e15c721e84a0882c5be199d545a
LogicodeBreeze <breezelogicode@gmail.com>
2026-03-12 23:59:48 +0100

    Add files via upload

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = "localhost";
+$db = "notas_app";
+$user = "root";
+$pass = "";
+
+try{
+
+$pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8",$user,$pass);
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+}catch(PDOException $e){
+
+echo "Error de conexión: ".$e->getMessage();
+
+}
